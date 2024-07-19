20823a828e9dda607df8a90da28d8d3d2f4cba2e
Carlos Perez <carlos.perez02@epn.edu.ec>
2024-07-19 12:41:45 -0500

    Formas de CRUD

--- src/Main.java
+++ src/Main.java
@@ -26,9 +26,45 @@ public class Main {
 
         // Leer
 
-        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017/miBaseDeDatos)")) {
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+
             MongoDatabase database = mongoClient.getDatabase("miBaseDeDatos");
             MongoCollection<Document> collection = database.getCollection("miColeccion");
+
+            FindIterable<Document> documentos = collection.find();
+
+            for (Document documento : documentos) {
+                //System.out.println(documento.toJson());
+                String nombre = documento.getString("nombre");
+                String apellido = documento.getString("apellido");
+                int edad = documento.getInteger("edad");
+
+                System.out.println("Nombre: " + nombre);
+                System.out.println("Apellido: " + apellido);
+                System.out.println("Edad: " + edad);
+            }
+        }
+
+        // Actualizar
+
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+            MongoDatabase database = mongoClient.getDatabase("miBaseDeDatos");
+            MongoCollection<Document> collection = database.getCollection("miColeccion");
+            Document filtro = new Document("nombre", "Yadira");
+            Document actualizacion = new Document("$set", new Document("apellido", "Gómez"));
+            UpdateResult resultado = collection.updateOne(filtro, actualizacion);
+            System.out.println("Documentos modificados: " + resultado.getModifiedCount());
+        }
+
+        //Borrar
+
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+
+            MongoDatabase database = mongoClient.getDatabase("miBaseDeDatos");
+            MongoCollection<Document> collection = database.getCollection("miColeccion");
+            Document filtro = new Document("nombre", "Carlos");
+            DeleteResult resultado = collection.deleteOne(filtro);
+            System.out.println("Documentos borrados: " + resultado.getDeletedCount());
         }
     }
 }
